5b46bdf36fe6ad871f2d7e96d0ddb837517ab1aa
Shadi-Basudan <114811335+Shadi-Basudan@users.noreply.github.com>
2023-01-30 21:11:20 +0300

    Update signup.php

--- iEventer demo 2/signup.php
+++ iEventer demo 2/signup.php
@@ -1,71 +1,59 @@
 ﻿<html>
 
-<head>
-    <title>Sign up</title>
+
     <link rel="stylesheet" href="style.css">
-    <script>
-        function tips(num) {
-            if (num == 1) {
-                document.getElementById("tips_section").innerHTML = "Tip:User name should be in English and contain upper and lower cases.";
-            }
-
-            if (num == 2) {
-                document.getElementById("tips_section").innerHTML = "Tip:Make sure to Enter email correctly, you will receive emails from the platrofm based on this email.";
-            }
-
-            if (num == 3) {
-                document.getElementById("tips_section").innerHTML = "Password should contain upper and lower casses and numbers and (8 characters minimum).";
-
+    <script type="text/javascript">
+        function checkInfo() {
+            var username = document.getElementById("name").value;
+            var email = document.getElementById("email").value;
+            var pw1 = document.getElementById("pw").value;
+            var pw2 = document.getElementById("pw2").value;
+            var pass_val = /^[A-Za-z]\w{7,14}$/;
+            if (username == "" || email == "" || pw1 == "" || pw2 == "") {
+                document.getElementById("checking").innerHTML = '<h1 style="margin: 8px 0; width: 100%; border-radius: 5px; padding: 10px 15px; text-align: center; background-color: red; color: white;font-size:20">You must filled all the fields</h1>';
+            } else {
+                document.getElementById("checking").innerHTML = '';
+                checkEmail();
+                checkPassword();
+                if ((email.endsWith('@stu.kau.edu.sa') || email.endsWith('@kau.edu.sa')) && pw1 == pw2 && pw1.length >= 8 && pw1.match(pass_val)) {
+                    document.getElementById("sub").style.display = "block";
+                    document.getElementById("name").style.borderColor = "black";
+                    document.getElementById("email").style.borderColor = "black";
+                    document.getElementById("pw").style.borderColor = "black";
+                    document.getElementById("pw2").style.borderColor = "black";
+                    document.getElementById("checking").innerHTML = '<h1 style="margin: 8px 0; width: 100%; border-radius: 5px; padding: 10px 15px; text-align: center; background-color: rgb(48, 170, 48); color: white;font-size:20">Valid information</h1>';
+                }
             }
         }
 
+        function checkEmail() {
+            var email = document.getElementById("email").value;
+            if (!(email.endsWith('@stu.kau.edu.sa') || email.endsWith('@kau.edu.sa'))) {
+                document.getElementById("checking").innerHTML = '<h1 style="margin: 8px 0; width: 100%; border-radius: 5px; padding: 10px 15px; text-align: center; background-color: red; color: white;font-size:20">email must end with @stu.kau.edu.sa</h1>';
+                document.getElementById("sub").style.display = "none";
+                document.getElementById("email").style.borderColor = "red";
+            } else {
+                document.getElementById("email").style.borderColor = "black";
+            }
+        }
 
-        function checkpassword() {
-            var pass_val = /^[A-Za-z]\w{7,14}$/;
+        function checkPassword() {
             var pw1 = document.getElementById("pw").value;
             var pw2 = document.getElementById("pw2").value;
-            var username = document.getElementById("name").value;
-            var email = document.getElementById("email").value;
-            if (pw1 != "" && pw2 != "" && username != "" && email != "" && pw1.length >= 8 && pw1.match(pass_val)) {
-                if (pw1 == pw2) {
-                    document.getElementById("checked").src = "check.jpg";
-                    document.getElementById("checked").width = 30;
-                    document.getElementById("checked").height = 30;
-                    document.getElementById("checked_sec").src = "check.jpg";
-                    document.getElementById("checked_sec").width = 30;
-                    document.getElementById("checked_sec").height = 30;
-                    document.getElementById("sub").style.display = "block";
-                }
-            } else {
+            var pass_val = /^[A-Za-z]\w{7,14}$/;
+            if (!(pw1 == pw2 && pw1.length >= 8 && pw1.match(pass_val))) {
                 document.getElementById("sub").style.display = "none";
-                document.getElementById("checked").src = "wrong.jpg";
-                document.getElementById("checked").width = 30;
-                document.getElementById("checked").height = 30;
-                document.getElementById("checked_sec").src = "wrong.jpg";
-                document.getElementById("checked_sec").width = 30;
-                document.getElementById("checked_sec").height = 30;
+                document.getElementById("pw").style.borderColor = "red";
+                document.getElementById("pw2").style.borderColor = "red";
+                document.getElementById("checking").innerHTML = '<h1 style="margin: 8px 0; width: 100%; border-radius: 5px; padding: 10px 15px; text-align: center; background-color: red; color: white;font-size:20">incorrect password</h1>';
+
+            } else {
+                document.getElementById("pw").style.borderColor = "black";
+                document.getElementById("pw2").style.borderColor = "black";
             }
         }
     </script>
-
-    <style>
-        #form_div {
-            position: absolute;
-            margin-top: 320px;
-            margin-left: 550px;
-            margin-bottom: 10px;
-            border: 0px;
-            width: 400px;
-            height: 150px;
-            box-shadow: 0 0 20px 0 rgb(104, 34, 34);
-            width: 600px;
-            height: 350px;
-            border-radius: 5px;
-            font-size: 25px;
-        }
-    </style>
-</head>
-<?php
+</head><?php
 $conn = mysqli_connect('localhost', 'root', '', 'ieventer');
 if ($conn) {
     echo "Connection sucsessful";
@@ -83,35 +71,68 @@ if (isset($_POST['sub'])) {
 ?>
 
 <body style="background-color:rgb(255, 255 255);">
-    <header>
+     <header>
         <a href="#" class="logo">iEventer</a>
     </header>
-
-    <div id="form_div">
-        <form name="form" method="POST">
-            <label>Username:</label>
-            <input type="text" id="name" name="uname" onfocus="tips(1)" required />
-            <br />
-            <label>Email:</label>
-            <input type="email" id="email" name="email" onfocus="tips(2)" required />
-            <br />
-            <label>Password:</label>
-            <input type="password" id="pw" name="password" onfocus="tips(3)" required />
-            <img id="checked"/>
-            <br />
-            <label>Confirm Password:</label>
-            <input type="password" id="pw2" name="pw2" required />
-            <img id="checked_sec" />
-            <input type="button" value="check info" id="checker" onclick="checkpassword()" required />
-            <br />
-            <br />
-            <br />
-            <a style="font-size:14px;" href="login.php">Have an account already?Sign in </a>
+    <div class="form-container">
+        <form action="" method="POST" name="form" id="form">
+            <div id="checking"></div>
+            <input name="uname" type="text" id="name" placeholder="Enter your name" required />
+            <input name="email" type="email" id="email" placeholder="Enter your email" required />
+            <input name="pass" type="password" id="pw" placeholder="Enter your password" required />
+            <input name="cpassword" type="password" id="pw2" placeholder="Confirm your password" required />
+            <input type="button" value="check info" id="checker" onclick="checkInfo()" required />
+            <p>already have an account? <a href="login.php">login now</a></p>
             <hr>
-            <input style="display:none" type="submit" id="sub" name="sub" value="Submit" required />
-            <br />
-            <div style="font-size:20px; color:darkblue" id="tips_section"></div>
+            <input style="display:none" type="submit" id="sub" name="sub" class="form-btn" value="register now" />
+            <div style="font-size:13px; color:darkblue">
+                <ul style="text-align: left; margin-top:3px; margin-left:15px">
+                    <li>Make sure to enter email ends with @stu.kau.edu.sa or @kau.edu.sa</li>
+                    <li>Password should contain upper, lower casses, numbers and (8 characters minimum)</li>
+                </ul>
+            </div>
         </form>
     </div>
+    <footer class="footer">
+        <p class="footer-title"><span>Copyrights @ </span>Mohammed Aljabarti and Shadi Basudan</p>
+    </footer>
+</body>
+
+<?php
+include 'config.php';
+if (isset($_POST['sub'])) {
+    $name = mysqli_real_escape_string($conn, $_POST['uname']);
+    $password = mysqli_real_escape_string($conn, $_POST['pass']);
+    $cpass = mysqli_real_escape_string($conn, $_POST['cpassword']);
+    $email = mysqli_real_escape_string($conn, $_POST['email']);
+    $std_domain = '@stu.kau.edu.sa';
+    $ins_domain = '@kau.edu.sa';
+    $default_image = 'images/default-avatar.png';
+    $select = mysqli_query($conn, "SELECT * FROM user WHERE email = '$email' AND password = '$password'") or die('query failed');
+    if (mysqli_num_rows($select) > 0) {
+        echo '';
+    } else {
+        if ($password != $cpass) {
+            echo 'confirm password not matched!';
+        } elseif ((endsWith($email, $std_domain) || endsWith($email, $ins_domain)) == false) {
+            echo 'email should ends with @stu.kau.edu.sa or @kau.edu.sa !';
+        } else {
+            $insert = mysqli_query($conn, "INSERT INTO user (id, name, email, password, gender, department, image,Notify) VALUES(NULL, '$name', '$email', '$password', 'NULL', 'NULL', '$default_image','Y')") or die('query failed');
+            header('location:login.php');
+            exit;
+        }
+    }
+}
+
+function endsWith($string, $endString)
+{
+    $len = strlen($endString);
+    if ($len == 0) {
+        return true;
+    }
+    return (substr($string, -$len) === $endString);
+}
+?>
+
 </body>
 </html>
