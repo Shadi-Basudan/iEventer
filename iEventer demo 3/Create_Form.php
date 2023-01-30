d547f142ac83a41679f2244a8088c450ba156b2e
Shadi-Basudan <114811335+Shadi-Basudan@users.noreply.github.com>
2023-01-30 22:01:58 +0300

    Update Create_Form.php

--- iEventer demo 3/Create_Form.php
+++ iEventer demo 3/Create_Form.php
@@ -173,6 +173,506 @@ a {
 
 
 
+<h2 id="deleting">Click To Delete Existed Forum!</h2>
+<img src="images/delete.png" alt="adding new forum" width=80 height=80 onclick="Delete_Forum()" id="addpicdel">
+
+
+
+
+
+
+<div >
+
+
+<br>
+
+ 
+
+
+
+
+<form method="POST" id="delete_forum" name="delete" style="display:none;" onclick="Delete_Forum()">
+
+
+<h2>Select from the Following existed forums to Delete!</h2>
+
+
+<br>
+
+<select name="deletion" >
+
+
+
+
+
+
+<?php
+
+
+
+
+$conn =mysqli_connect('localhost','root','','ieventer');
+
+
+if ($conn) {
+
+
+echo "Connection sucsessful";
+ }
+ else {
+
+ echo "error";
+ }
+
+ $name=$_SESSION['name'];
+
+
+ $sql="SELECT * from form_cat where name ='$name';
+" ;
+;
+ 
+
+$loop = mysqli_query($conn,$sql);
+
+
+
+while($row = mysqli_fetch_array($loop)){
+
+ 
+    $subject=$row['Subject_name'];
+    $pic=$row['pic'];
+
+  echo "
+
+
+  <option value='$subject'>$subject</option>
+";
+
+ 
+
+   
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+?>
+</select>
+<br>
+<br>
+
+<input name="del" type="submit" class="btn btn-dark" value="Apply" onclick="hide('deleting','delete_forum','deleting')"  >
+
+</form>
+
+
+
+
+
+<hr>
+
+<h2 id="editing">Click To Edit Existed Forum!</h2>
+<img src="images/edit.png" alt="edit new forum" width=80 height=80 onclick="Edit_Forum()" id="edit">
+
+
+<!--Edit feature-->
+
+ 
+<form method="POST" id="edit_forum" name="edit" style="display:none;" onclick="Edit_Forum()">
+
+
+<h2>Select from the Following existed forums to Edit!</h2>
+
+
+<br>
+
+<select name="editing" >
+
+
+
+
+
+
+<?php
+
+
+
+
+$conn =mysqli_connect('localhost','root','','ieventer');
+
+
+if ($conn) {
+
+
+echo "Connection sucsessful";
+ }
+ else {
+
+ echo "error";
+ }
+
+ $name=$_SESSION['name'];
+
+
+ $sql="SELECT * from form_cat where name ='$name';
+" ;
+;
+ 
+
+$loop = mysqli_query($conn,$sql);
+
+
+
+while($row = mysqli_fetch_array($loop)){
+
+ 
+    $subject=$row['Subject_name'];
+    $pic=$row['pic'];
+
+  echo "
+
+
+  <option value='$subject'>$subject</option>
+";
+
+ 
+
+   
+
+
+}
+
+
+
+
+
+
+
+?>
+</select>
+<br>
+<br>
+
+
+
+
+<h3>The new Name:</h3>
+
+
+
+<br>
+ <input name="edited"   type='text'>
+<br>
+
+<br>
+
+
+
+<h3>The New  Category:</h3>
+
+<select name="selct_to_edit" id="selection">
+
+
+<option value="Programming">Programming</option>
+
+<option value="Regelgion">Religion</option>
+
+<option value="Sport">Sport</option>
+
+<option value="Technical">Technical</option>
+
+<option value="Other">Other</option>
+
+
+
+
+
+
+
+
+
+</select>
+
+
+<br>
+
+
+<br>
+
+<input name="edit" type="submit"  class="btn btn-dark" value="Apply" onclick="hide('edit','edit','editing')"  >
+
+</form>
+
+
+
+
+
+
+<?php
+
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+
+$conn =mysqli_connect('localhost','root','','ieventer');
+
+
+if ($conn) {
+
+
+echo "Connection sucsessful";
+ }
+ else {
+
+ echo "error";
+ }
+
+
+ 
+ 
+
+ if(isset($_POST["edit"])){ 
+
+
+
+
+  $old_name=$_POST['editing'];
+  
+
+  $new_name=$_POST['edited'];
+
+  if($new_name==''){
+
+
+    $new_name=$old_name;
+
+  }
+
+  $category=$_POST['selct_to_edit'];
+
+
+
+  $sql="UPDATE form_cat
+  SET Subject_name = '$new_name', Category= '$category'
+  WHERE Subject_name = '$old_name';";
+  try{
+  mysqli_query($conn,$sql);
+
+    
+ 
+
+  }
+  
+  
+  catch(err){
+  
+  }
+
+
+  echo "<script> msg_del_edit()</script>";
+
+
+ 
+   
+//header("Location:Create_Form.php");
+//ob_end_flush();
+
+ 
+
+ }
+
+
+
+
+
+ if(isset($_POST["del"])){ 
+
+ 
+  $seleced=$_POST['deletion'];
+
+ 
+  $sql="DELETE FROM `form_cat` WHERE Subject_name='$seleced';
+  ";
+  try{
+  mysqli_query($conn,$sql);
+  }
+  
+  
+  catch(err){
+  
+  }
+  
+
+  echo "<script> msg_del_edit()</script>";
+  
+//header("Location:Create_Form.php");
+//ob_end_flush();
+
+ 
+
+ }
+
+
+
+
+
+
+
+ if(isset($_POST["sub"])){
+
+
+
+
+
+    $name=$_SESSION['name'];
+
+    $category=$_POST['selct'];
+
+
+    $subject=$_POST['subject'];
+
+    $_SESSION['subject'] = $subject;
+
+
+ 
+    $filename = $_FILES["file"]["name"];
+    
+    $tempname = $_FILES["file"]["tmp_name"];
+    $folder = "images/" . $filename;
+    move_uploaded_file($filename, $folder);
+
+
+
+    $date=date("Y/m/d")." ".date("l")."";
+$sql="INSERT INTO `form_cat` (`Name`, `Category`, `Subject_name`, `Date`,`pic`) VALUES ('$name', '$category', '$subject', '$date','$folder');";
+
+try{
+mysqli_query($conn,$sql);
+}
+
+
+catch(err){
+
+}
+
+
+ 
+
+
+
+
+
+
+
+function Send_Notification($email){
+
+
+require_once "vendor/autoload.php";
+
+//PHPMailer Object
+$mail = new PHPMailer(true); //Argument true in constructor enables exceptions
+
+$mail->IsSMTP();
+$mail->Host = "smtp.gmail.com";
+
+$mail->SMTPAuth = "true";
+$mail->SMTPSecure = "tls";
+$mail->Port = "587";
+// optional
+// used only when SMTP requires authentication  
+$mail->SMTPAuth = true;
+$mail->Username = 'ieventersm11@gmail.com
+
+';
+$mail->Password = 'ztdyqzlpujlkqifd';
+
+//From email address and name
+$mail->From = "ieventersm11@gmail.com
+
+";
+$mail->FromName = "iEventer";
+
+//To address and name
+$mail->addAddress($email); //Recipient name is optional
+
+//Address to which recipient will reply
+//$mail->addReplyTo("reply@yourdomain.com", "Reply");
+
+/*CC and BCC
+$mail->addCC("cc@example.com");
+$mail->addBCC("bcc@example.com");
+*/
+
+//Send HTML or Plain Text email
+$mail->isHTML(true);
+
+$mail->Subject = "Forums";
+$mail->Body = "<b> A New forum is available Created by ".$_SESSION['name']." Discussing {' ".$_SESSION['subject']." '}";
+$mail->AltBody = "This is the plain text version of the email content";
+
+try {
+    $mail->send();
+    echo "Message has been sent successfully";
+} catch (Exception $e) {
+    echo "Mailer Error: --> " . $mail->ErrorInfo;
+}
+
+$mail->smtpClose();
+
+}
+
+
+
+$sql="SELECT * from users;";
+    
+     
+    
+$loop = mysqli_query($conn,$sql);
+
+
+
+while($row = mysqli_fetch_array($loop)){
+
+ 
+    $email=$row['email'];
+
+    Send_Notification($email);
+
+ 
+ }    
+
+
+
+
+
+header("Location:Create_Form.php");
+ob_end_flush();
+
+
+     
+
+
+ }
+
+ 
+ 
+
+?>
+
+
+
+
+</div>
 
 
 
